d91bc55b141e9e6bd67e31562368117752b3acdf
JH <jiho4890@gmail.com>
2026-03-05 16:35:37 +0900

    feat: 동물과 놀기 기능 구현

--- src/zoo/app/Main.java
+++ src/zoo/app/Main.java
@@ -28,8 +28,18 @@ public class Main {
                 case 2: // 동물 목록 조회
                     printAnimalList();
                     break;
+                case 3: // 동물과 놀기
+                    playAnimal();
+                    break;
+                case 4:
 
+                    break;
+                case 5:
 
+                    break;
+                case  6:
+
+                    break;
                 case 7: //종료
                     running = false;
                     System.out.println("프로그램을 종료합니다.");
@@ -61,23 +71,53 @@ public class Main {
 
         Animal animal = (type == 1) ? new Dog(name, age) : new Cat(name, age);
         ZOO.addAnimal(animal);
-        System.out.printf("%s(%s, %d살)가 등록되었습니다.\n", animal.getName(), animal.getType(), animal.getAge());
+        System.out.printf("%s(%s, %d살)(이)가 등록되었습니다.\n", animal.getName(), animal.getType(), animal.getAge());
     }
 
     // 동물 목록 조회
     private static void printAnimalList() {
+        // 목록이 비어있는지 확인
         if(!ZOO.checkAnimal()) {
             System.out.println("등록된 동물이 없습니다.");
             return;
         }
 
+        // 동물 목록 출력
         System.out.println("=== 동물 목록 ===");
         for (int i = 0; i < ZOO.getAnimals().size(); i++){
-            Animal animal = ZOO.getAnimals().get(i);
+            Animal animal = ZOO.getAnimalIndex(i);
             System.out.printf("%d. %s(%s, %d살)\n", i+1, animal.getName(), animal.getType(), animal.getAge() );
         }
     }
 
+    // 동물과 놀기
+    private static void playAnimal(){
+        System.out.println("놀아줄 동물을 선택하세요.");
+
+        // 목록이 비어있는지 확인
+        if(!ZOO.checkAnimal()) {
+            System.out.println("등록된 동물이 없습니다.");
+            return;
+        }
+
+        // 동물 목록 출력
+        for (int i = 0; i < ZOO.getAnimals().size(); i++){
+            Animal animal = ZOO.getAnimals().get(i);
+            System.out.printf("%d. %s(%s, %d살)\n", i+1, animal.getName(), animal.getType(), animal.getAge() );
+        }
+
+        // 입력
+        int choice = SCANNER.nextInt();
+        Animal select = ZOO.getAnimalIndex(choice - 1);
+
+        // 선택한 동물과 놀기
+        if (select == null) {
+            System.out.println("잘못된 번호입니다.");
+        } else {
+            select.play();
+            System.out.printf("%s와(과) 놀았습니다. 행복도가 증가했습니다\n", select.getName());
+        }
+    }
 
 
 }
